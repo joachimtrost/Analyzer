edcf608ba97c1b55ccafb85b2b4904c37507f5ec
Joachim Trost <joachim.trost@bluewin.ch>
2018-10-03 06:30:35 +0200

    functioning simple tail functionality

--- src/main/java/org/jt/poker/App.java
+++ src/main/java/org/jt/poker/App.java
@@ -1,13 +1,30 @@
 package org.jt.poker;
 
+import org.jt.poker.filewatcher.FileWatcher;
+
+import java.io.File;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private final static Logger LOGGER = Logger.getLogger(App.class.getName());
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try {
+//            URI uri  = new URI("file:///C:/Users/joach/AppData/Local/PokerStars/HandHistory/Beaker103/HH20180923%20Halley%20-%20%240.01-%240.02%20-%20USD%20No%20Limit%20Hold%27em.txt") ;
+            URI uri  = new URI("file:///C:/temp/test.txt") ;
+            File file = new File(uri) ;
+            FileWatcher fw = new FileWatcher(file,100) ;
+            fw.run();
+        }
+        catch(java.net.URISyntaxException ex) {
+            LOGGER.log(Level.SEVERE, "URISyntaxException",ex) ;
+        }
+
+
     }
 }
---
+++ src/main/java/org/jt/poker/filewatcher/FileWatcher.java
@@ -0,0 +1,136 @@
+package org.jt.poker.filewatcher;
+
+/**
+ * Copyright 2013 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+//       import org.eclipse.jetty.websocket.api.RemoteEndpoint;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+
+import static java.lang.Math.max;
+import static java.lang.Thread.sleep;
+import static java.nio.ByteBuffer.allocate;
+import static java.nio.file.Files.newByteChannel;
+import static java.nio.file.StandardOpenOption.READ;
+import static org.apache.commons.io.IOUtils.closeQuietly;
+
+/**
+ * A non-blocking tail implementation allowing to read an arbitrary number of bytes from the end of a file
+ * and follow changes to it.
+ *
+ * @author Olaf Otto
+ */
+public class FileWatcher implements Runnable {
+    private static final int AWAIT_FILE_ROTATION_MILLIS = 1000;
+    private static final int TAIL_CHECK_INTERVAL_MILLIS = 500;
+
+    private final Logger LOGGER = Logger.getLogger(FileWatcher.class.getName());
+
+    //    private final RemoteEndpoint remoteEndpoint;
+    private final File file;
+    private final long bytesToTail;
+
+    private boolean stopped = false;
+
+    //    Tail(RemoteEndpoint remoteEndpoint, File file, long bytesToTail) {
+    public FileWatcher(File file, long bytesToTail) {
+        if (file == null) {
+            throw new IllegalArgumentException("constructor parameter file must not be null");
+        }
+        this.bytesToTail = bytesToTail;
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        SeekableByteChannel channel = null;
+
+        try {
+            channel = newByteChannel(this.file.toPath(), READ);
+
+            long availableInByte = this.file.length();
+            long startingFromInByte = max(availableInByte - this.bytesToTail, 0);
+
+            channel.position(startingFromInByte);
+
+            long position = startingFromInByte;
+
+            // Read up to this amount of data from the file at once.
+            ByteBuffer readBuffer = allocate(4096);
+            while (!this.stopped) {
+
+                // The file might be temporarily gone during rotation. Wait, then decide
+                // whether the file is considered gone permanently or whether a rotation has occurred.
+                if (!this.file.exists()) {
+                    sleep(AWAIT_FILE_ROTATION_MILLIS);
+                }
+                if (!this.file.exists()) {
+                    LOGGER.severe("file not found");
+                    return;
+                }
+
+                if (position > this.file.length()) {
+                    LOGGER.info("file rotated");
+                    position = 0;
+                    closeQuietly(channel);
+                    channel = newByteChannel(this.file.toPath(), READ);
+                }
+
+                int read = channel.read(readBuffer);
+
+                if (read == -1) {
+                    sleep(TAIL_CHECK_INTERVAL_MILLIS);
+                    continue;
+                }
+
+                position = channel.position();
+
+                readBuffer.flip();
+
+                LOGGER.info(String.format("next chars %s %d",readBuffer.toString(), read));
+                StringBuffer sb = new StringBuffer() ;
+                for(int i=0 ; i<(read) ; i++) {
+                    sb.append((char)(readBuffer.get())) ;
+//                    sb.append(" ") ;
+                }
+
+                LOGGER.info(String.format("/%s/",sb.toString()));
+                readBuffer.clear();
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE,"Unable to tail " + this.file.getAbsolutePath() + ".", e);
+        } catch (InterruptedException e) {
+            if (!this.stopped) {
+                LOGGER.log(Level.SEVERE,"Stopped tailing " + this.file.getAbsolutePath() + ", got interrupted.", e);
+            }
+        } finally {
+            closeQuietly(channel);
+        }
+    }
+
+    public void stop() {
+        this.stopped = true;
+    }
+}
+
+
+
+
+
